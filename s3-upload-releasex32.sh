422be9509bf7c74f3dcf74a518e9d7cb671783af
Soumyajit Dutta <sanudatta11@gmail.com>
2018-07-09 13:56:17 +0530

    Added CI For all platforms with s3 build upload
    Linux Upload CI Commands added
    
    Added mac ci s3 upload
    
    Added python install
    
    Added python install 2
    
    Added python install 3
    
    Added python install 3
    
    Added python install 3
    
    Added python install 4
    
    Added python install 5
    
    Added python install 6
    
    Added python install 7
    
    Added python install 7
    
    Added python install 7
    
    Added python install 7
    
    Added python install 8
    
    Added aws cli installation appveyor
    
    Added aws cli installation appveyor:yaml fi
    
    Added aws cli installation appveyor:yaml fix
    
    Added aws cli upload
    
    Added aws cli upload 2
    
    Added aws cli upload 3
    
    Bash CLI Fix
    
    Added Windows build fixed
    
    
    Added new lines for builds
    
    yaml correction
    
    invalid init correction
    
    Added electron builder
    
    Added electron builder 2
    
    Added electron builder 3
    
    Added electron builder 3
    
    Added electron builder 4
    
    Added electron builder 5
    
    Added electron builder 6
    
    Added windows s3 upload
    
    Added nizam's email wwith correct config

---
+++ s3-upload-releasex32.sh
@@ -0,0 +1,3 @@
+aws s3 cp *.exe s3://ana-content/cdn/dist/agent-panel/win-ia32/
+aws s3 cp *.exe.blockmap s3://ana-content/cdn/dist/agent-panel/win-ia32/
+aws s3 cp *.yaml s3://ana-content/cdn/dist/agent-panel/win-ia32/
---
+++ s3-upload-releasex64.sh
@@ -0,0 +1,3 @@
+aws s3 cp *.exe s3://ana-content/cdn/dist/agent-panel/win-x64/
+aws s3 cp *.exe.blockmap s3://ana-content/cdn/dist/agent-panel/win-x64/
+aws s3 cp *.yaml s3://ana-content/cdn/dist/agent-panel/win-x64/
