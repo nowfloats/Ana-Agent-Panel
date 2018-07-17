aa040f2b8f73552a06bdc08ff546642718aae355
Soumyajit Dutta <sanudatta11@gmail.com>
2018-07-17 20:18:37 +0530

    removed unecessary files

--- s3-upload-releasex32.sh
+++
@@ -1,3 +0,0 @@
-aws s3 cp *.exe s3://ana-content/cdn/dist/agent-panel/win-ia32/
-aws s3 cp *.exe.blockmap s3://ana-content/cdn/dist/agent-panel/win-ia32/
-aws s3 cp *.yaml s3://ana-content/cdn/dist/agent-panel/win-ia32/
